57096bb6679ac0ebc2952f03da2946441b3a9f6c
SaraMCG <84095824+SaraMCG@users.noreply.github.com>
2022-05-23 17:33:07 -0500

    [ADD] Companion object

---
+++ src/CompanionObject.kt
@@ -0,0 +1,9 @@
+open class CompanionObject {
+
+    companion object {
+        public const val ERROR_MESSAGE = "Error al realizar la acción"
+        public const val  SUCCESS_MESSAGE = "Acción realizada exitosamente"
+        public const val INFO_MESSAGE = "Advertencia! Id ya registrado"
+    }
+
+}
