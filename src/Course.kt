ed44d60c84fa184d7d633448ec3d6e587df58445
David <davidperez@ute.edu.mx>
2022-05-23 18:14:23 -0500

    UPDATE

--- src/Course.kt
+++ src/Course.kt
@@ -1,7 +1,7 @@
 import java.util.UUID
 import java.util.UUID.randomUUID
 
-open class Course (var id : Int? = 0, var name : String = "", var description : String = ""){
+open class Course (var name : String = "", var description : String = "",var id : Int? = 0){
 
     init {
         id = Integer.parseInt(randomUUID().toString())
